0e0dfdd5f72aab7715bc2548ee52dcfc4c904bff
iris <carlim038@gmail.com>
2026-08-06 18:56:03 +0200

    Primo commit: aggiunta file del progetto

--- main.js
+++ main.js
@@ -3,17 +3,19 @@ let navbar = document.querySelector('.navbar');
 let links = document.querySelectorAll('.nav-link');
 let logoNavbar = document.querySelector('#logoNavbar');
 
-window.addEventListener('scroll', () => {
-    let scrolled = window.scrollY;
+if (navbar && logoNavbar) {
+    window.addEventListener('scroll', () => {
+        let scrolled = window.scrollY;
 
-    if (scrolled > 0) {
-        navbar.classList.add('navbar-scrolled');
-        logoNavbar.src = './media/logo nero.png';
-    } else {
-        navbar.classList.remove('navbar-scrolled');
-        logoNavbar.src = './media/logo giallo.png';
-    }
-});
+        if (scrolled > 0) {
+            navbar.classList.add('navbar-scrolled');
+            logoNavbar.src = './media/logo nero.png';
+        } else {
+            navbar.classList.remove('navbar-scrolled');
+            logoNavbar.src = './media/logo giallo.png';
+        }
+    });
+}
 
 // --- 2. Numeri incrementali e Intersection Observer ---
 let firstNumber = document.querySelector("#firstNumber");
@@ -41,26 +43,28 @@ function createInterval(n, element, time) {
 }
 
 let triggerElement = document.querySelector("#firstNumber");
-let observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        if (entry.isIntersecting && confirmCheck) {
-            createInterval(100, firstNumber, 100);
-            createInterval(200, secondNumber, 50);
-            createInterval(300, thirdNumber, 30);
-            observer.unobserve(entry.target);
-        }
-    });
-});
-
 if (triggerElement) {
+    let observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting && confirmCheck) {
+                createInterval(100, firstNumber, 100);
+                createInterval(200, secondNumber, 50);
+                createInterval(300, thirdNumber, 30);
+                observer.unobserve(entry.target);
+            }
+        });
+    });
     observer.observe(triggerElement);
 }
 
 // --- 3. Inizializzazione Swiper ---
-var swiper = new Swiper('.swiper', {
-    effect: 'flip',
-    grabCursor: true,
-});
+let swiperContainer = document.querySelector('.swiper');
+if (swiperContainer) {
+    var swiper = new Swiper('.swiper', {
+        effect: 'flip',
+        grabCursor: true,
+    });
+}
 
 // --- 4. Gestione Recensioni e Stelline Dinamiche ---
 let reviews = [
@@ -70,19 +74,43 @@ let reviews = [
 ];
 
 let allReviewsCards = document.querySelectorAll('.card-review');
-
-allReviewsCards.forEach((card, index) => {
-    let starsContainer = card.querySelector('.stars');
-    
-    if (starsContainer) {
-        starsContainer.innerHTML = ''; 
-        let reviewData = reviews[index];
-        if (reviewData) {
-            for (let i = 1; i <= reviewData.stars; i++) {
-                let icon = document.createElement("i");
-                icon.className = "fa-solid fa-star text-warning";
-                starsContainer.appendChild(icon);
+if (allReviewsCards.length > 0) {
+    allReviewsCards.forEach((card, index) => {
+        let starsContainer = card.querySelector('.stars');
+        
+        if (starsContainer) {
+            starsContainer.innerHTML = ''; 
+            let reviewData = reviews[index];
+            if (reviewData) {
+                for (let i = 1; i <= reviewData.stars; i++) {
+                    let icon = document.createElement("i");
+                    icon.className = "fa-solid fa-star text-warning";
+                    starsContainer.appendChild(icon);
+                }
             }
         }
+    });
+}
+
+// --- 5. Effetto apertura cerchi Chi Siamo ---
+let opener = document.querySelector('.opener');
+let movedDivs = document.querySelectorAll('.moved');
+
+let check = false;
+
+opener.addEventListener('click', ()=> {
+    if(check == false) {
+        opener.style.transform = 'rotate(45deg)';
+        movedDivs.forEach((moved, i)=> {
+            let angle = (360 * i) / movedDivs.length;
+            moved.style.transform = `rotate(${angle}deg) translate(150px)`;
+        });
+        check = true;
+    } else {
+        opener.style.transform = 'rotate(0deg)';
+        movedDivs.forEach((moved)=> {
+            moved.style.transform = `rotate(0deg) translate(0px)`;
+        });
+        check = false;
     }
 });
